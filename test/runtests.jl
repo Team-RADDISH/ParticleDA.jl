66499945151e6019dc6c19f9a7ce08039a50c633
Tuomas Koskela <t.koskela@ucl.ac.uk>
2020-03-10 15:42:18 +0000

    More tests, removed @show

--- test/runtests.jl
+++ test/runtests.jl
@@ -154,6 +154,7 @@ end
     # 0 input gives 0 output
     x0 = x = zeros(nx * ny * 3)
     gg, hh, hm, hn, fm, fn, fe = TDAC.LLW2d.setup(nx,ny,3.0e4)
+    @test size(gg) == size(hh) == size(hm) == size(fm) == size(fn) == size(fe) == (nx,ny)
     TDAC.tsunami_update!(x, nx, ny, dx, dy, dt, hm, hn, fm, fn, fe, gg)
     @test x ≈ x0
 
@@ -164,7 +165,6 @@ end
     @test eta[2,2] ≈ 1.0
     @test sum(eta) ≈ 4.0
     TDAC.tsunami_update!(x, nx, ny, dx, dy, dt, hm, hn, fm, fn, fe, gg)
-    show(eta)
     @test sum(eta, dims=1) ≈ [0.9140901416339269 1.7010577375770561 0.9140901416339269 0.06356127284539884 0.0 0.0 0.0 0.0 0.0 0.0]
     @test sum(eta, dims=2) ≈ [0.9068784611641829; 1.6999564781646717; 0.9204175965604575; 0.06554675780099671; 0.0; 0.0; 0.0; 0.0; 0.0; 0.0]
 end
