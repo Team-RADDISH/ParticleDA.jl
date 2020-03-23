0e99cd8c51a0cc2002f316334f6dd89c7db33bff
Tuomas Koskela <t.koskela@ucl.ac.uk>
2020-03-23 15:10:53 +0000

    Bug fix, initheight was called on the wrong state array

--- src/TDAC.jl
+++ src/TDAC.jl
@@ -202,7 +202,7 @@ function tdac(params)
     gg, hh, hm, hn, fm, fn, fe = LLW2d.setup(params.nx, params.ny, params.bathymetry_setup)
 
     # obtain initial tsunami height
-    eta = reshape(@view(state[1:params.dim_grid]), params.nx, params.ny)
+    eta = reshape(@view(state_true[1:params.dim_grid]), params.nx, params.ny)
     LLW2d.initheight!(eta, hh, params.dx, params.dy, params.source_size)
 
     # set station positions
