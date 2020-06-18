056633c6b344eb4627efc10c03f296524a611ef2
Tuomas Koskela <t.koskela@ucl.ac.uk>
2020-06-18 11:09:26 +0100

    Add tests for particle state

--- test/runtests.jl
+++ test/runtests.jl
@@ -191,18 +191,30 @@ end
     attr = h5readattr(params.output_filename, params.title_stations * "/y")
     @test attr["Unit"] == "m"
 
-    rm(params.output_filename, force=true)    
+    rm(params.output_filename, force=true)
 end
 
 @testset "TDAC integration tests" begin
 
-    x_true,x_avg,v_var = TDAC.tdac(joinpath(@__DIR__, "integration_test_1.yaml"))
+    # Test true state with standard parameters
+    x_true,x_avg,x_var = TDAC.tdac(joinpath(@__DIR__, "integration_test_1.yaml"))
     data_true = h5read(joinpath(@__DIR__, "reference_data.h5"), "integration_test_1")
     @test x_true[:] ≈ data_true
 
-    x_true,x_avg,v_var = TDAC.tdac(joinpath(@__DIR__, "integration_test_2.yaml"))
+    # Test true state with different parameters
+    x_true,x_avg,x_var = TDAC.tdac(joinpath(@__DIR__, "integration_test_2.yaml"))
     data_true = h5read(joinpath(@__DIR__, "reference_data.h5"), "integration_test_2")
     @test x_true ≈ data_true
+
+    # Test particle state with ~zero noise
+    x_true,x_avg,x_var = TDAC.tdac(joinpath(@__DIR__, "integration_test_3.yaml"))
+    @test x_true ≈ x_avg
+    @test x_var .+ 1.0 ≈ ones(size(x_var))
+
+    # Test particle state with noise
+    x_true,x_avg,x_var = TDAC.tdac(joinpath(@__DIR__, "integration_test_4.yaml"))
+    @test isapprox(x_true, x_avg, rtol=1e-2)
+    @test x_var .+ 1.0 ≈ ones(size(x_var))
 
 end
 
