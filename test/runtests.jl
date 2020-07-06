dd7d1ed8c11fa49d8a743b7b15255914846cd11c
Mosè Giordano <mose@gnu.org>
2020-07-06 11:39:41 +0100

    Fix some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -81,16 +81,20 @@ end
     weights = Vector{Float64}(undef, 3)
     # model observations with equal distance from true observation return equal weights
     hx = [0.5 0.9 1.5; 2.1 2.5 1.9]
-    TDAC.get_weights!(weights, y, hx, cov_obs)
+    TDAC.get_log_weights!(weights, y, hx, cov_obs)
+    @test weights ≈ [-1.9678770664093457, -1.9678770664093457, -1.9678770664093457]
+    TDAC.normalized_exp!(weights)
     @test weights ≈ ones(3) / 3
     # model observations with decreasing distance from true observation return decreasing weights
     hx = [0.9 0.5 1.5; 2.1 2.5 3.5]
-    TDAC.get_weights!(weights, y, hx, cov_obs)
+    TDAC.get_log_weights!(weights, y, hx, cov_obs)
+    TDAC.normalized_exp!(weights)
     @test weights[1] > weights[2] > weights[3]
 
     # multivariate and independent methods give same weights when covariance matrix is diagonal
     weights2 = Vector{Float64}(undef, 3)
-    TDAC.get_weights!(weights2, y, hx, 1.0)
+    TDAC.get_log_weights!(weights2, y, hx, 1.0)
+    TDAC.normalized_exp!(weights2)
     @test weights2 ≈ weights
 
     id = zeros(Int, 5)
