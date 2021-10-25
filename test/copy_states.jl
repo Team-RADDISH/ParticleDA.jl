ba649c702557dfdb9c6d4235a9cf1a98989a609d
Mosè Giordano <mose@gnu.org>
2021-10-25 14:33:11 +0100

    Add tests for generic `copy_states!` and `get_mean_and_var!`

--- test/copy_states.jl
+++ test/copy_states.jl
@@ -7,34 +7,38 @@ my_size = MPI.Comm_size(MPI.COMM_WORLD)
 nprt_per_rank = 3
 n = nprt_per_rank * my_size
 
-local_particles = float(collect(my_rank * nprt_per_rank + 1 : (my_rank + 1) * nprt_per_rank))
+@testset for size in ((1,1,1,nprt_per_rank), (1,1,1,1,nprt_per_rank))
 
-for i = 1:my_size
-    if i == my_rank + 1
-        println("rank ",my_rank,": particle states: ", local_particles)
+    local_particles = float(collect(my_rank * nprt_per_rank + 1 : (my_rank + 1) * nprt_per_rank))
+
+    for i = 1:my_size
+        if i == my_rank + 1
+            println("rank ",my_rank,": particle states: ", local_particles)
+        end
+        MPI.Barrier(MPI.COMM_WORLD)
     end
-    MPI.Barrier(MPI.COMM_WORLD)
-end
 
-buffer = zeros(1,1,1,nprt_per_rank)
+    buffer = zeros(size)
 
-Random.seed!(1234)
-indices = rand(1:n, n)
+    Random.seed!(1234)
+    indices = rand(1:n, n)
 
-if my_rank == 0
-    println()
-    println("Resampling particles to indices ", indices)
-    println()
-end
-
-ParticleDA.copy_states!(reshape(local_particles, (1,1,1,nprt_per_rank)), buffer, indices, my_rank, nprt_per_rank)
-
-for i = 1:my_size
-    if i == my_rank + 1
-        test = local_particles ≈ float(indices[my_rank * nprt_per_rank + 1 : (my_rank + 1) * nprt_per_rank])
-        println("rank ",my_rank,": particle states: ", local_particles, " -- ",test)
+    if my_rank == 0
+        println()
+        println("Resampling particles to indices ", indices)
+        println()
     end
-    MPI.Barrier(MPI.COMM_WORLD)
-end
 
-@test local_particles ≈ float(indices[my_rank * nprt_per_rank + 1 : (my_rank + 1) * nprt_per_rank])
+    ParticleDA.copy_states!(reshape(local_particles, size), buffer, indices, my_rank, nprt_per_rank)
+
+    for i = 1:my_size
+        if i == my_rank + 1
+            test = local_particles == float(indices[my_rank * nprt_per_rank + 1 : (my_rank + 1) * nprt_per_rank])
+            println("rank ",my_rank,": particle states: ", local_particles, " -- ",test)
+        end
+        MPI.Barrier(MPI.COMM_WORLD)
+    end
+
+    @test local_particles == float(indices[my_rank * nprt_per_rank + 1 : (my_rank + 1) * nprt_per_rank])
+
+end
