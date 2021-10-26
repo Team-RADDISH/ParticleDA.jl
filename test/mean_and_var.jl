c130ec02a2d7e922ed0fc4f5c61e1c6f3c6dda60
Mosè Giordano <giordano@users.noreply.github.com>
2021-10-26 10:33:06 +0100

    Generalise some dimensions in bootstrap filter (#201)
    * Generalise some dimensions in bootstrap filter
    
    * Add tests for generic `copy_states!` and `get_mean_and_var!`

--- test/mean_and_var.jl
+++ test/mean_and_var.jl
@@ -18,18 +18,23 @@ for i = 1:my_size
     MPI.Barrier(MPI.COMM_WORLD)
 end
 
-stats = Array{ParticleDA.SummaryStat{Float64},3}(undef, 1, 1, 1)
-ParticleDA.get_mean_and_var!(stats,reshape(local_particles,1,1,1,nprt_per_rank),0)
+@testset for size in ((1,1,1), (1,1,1,1))
 
-global_particles = MPI.Gather(local_particles, 0, MPI.COMM_WORLD)
+    stats = Array{ParticleDA.SummaryStat{Float64},length(size)}(undef, size)
+    ParticleDA.get_mean_and_var!(stats,reshape(local_particles,size...,nprt_per_rank),0)
+    @test_throws ErrorException ParticleDA.get_mean_and_var!(stats,reshape(local_particles,size[begin:end-1]...,nprt_per_rank),0)
 
-if my_rank == 0
-    gather_mean = Statistics.mean(global_particles)
-    gather_var = Statistics.var(global_particles, corrected=true)
+    global_particles = MPI.Gather(local_particles, 0, MPI.COMM_WORLD)
 
-    println("Mean     ",stats[1].avg, " -- ", stats[1].avg ≈ gather_mean)
-    println("Variance ",stats[1].var, " -- ", stats[1].var ≈ gather_var)
+    if my_rank == 0
+        gather_mean = Statistics.mean(global_particles)
+        gather_var = Statistics.var(global_particles, corrected=true)
 
-    @test stats[1].avg ≈ gather_mean
-    @test stats[1].var ≈ gather_var
+        println("Mean     ",stats[1].avg, " -- ", stats[1].avg ≈ gather_mean)
+        println("Variance ",stats[1].var, " -- ", stats[1].var ≈ gather_var)
+
+        @test stats[1].avg ≈ gather_mean
+        @test stats[1].var ≈ gather_var
+    end
+
 end
