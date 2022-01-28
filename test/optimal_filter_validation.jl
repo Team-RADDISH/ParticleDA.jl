37e68f86b2e1fcbc1162b7378910263bbbffd548
Matt Graham <matthew.m.graham@gmail.com>
2022-01-28 09:25:50 +0800

    Use hypot instead of norm
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- test/optimal_filter_validation.jl
+++ test/optimal_filter_validation.jl
@@ -98,7 +98,7 @@ Sobs = 0.01
 #### Example Specification of Covariance Function
 function R(x::Float64, y::Float64, th)
 
-  Cov = (th.s^2)*exp(-norm([x, y])/th.l)
+  Cov = (th.s^2)*exp(-hypot(x, y) / th.l)
 
   return(Cov)
 
