7538aef592dd2fcd3f50dbd489f36927c7bf8765
Matt Graham <matthew.m.graham@gmail.com>
2022-01-28 09:33:17 +0800

    Update type signature in docstring of get_model_noise
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- src/ParticleDA.jl
+++ src/ParticleDA.jl
@@ -50,7 +50,7 @@ Return standard deviation of observation noise. Required for optimal filter only
 function get_obs_noise_std end
 
 """
-    ParticleDa.get_model_noise(model_data) -> CovarianceStructure
+    ParticleDa.get_model_noise(model_data) -> GaussianRandomFields.IsotropicCovarianceStructure
 
 Return structure defining parameters of the covariance function of the state noise in
 the observed state variable. Required for optimal filter only.
