c8c51b5f6c3800d3e75f619cb4a24366be0f258a
Matt Graham <matthew.m.graham@gmail.com>
2022-11-23 21:51:55 +0000

    Rationalize filter parameters

--- src/params.jl
+++ src/params.jl
@@ -8,18 +8,17 @@ export FilterParameters
 Parameters for ParticleDA run. Keyword arguments:
 
 * `master_rank` : Id of MPI rank that performs serial computations
-* `n_time_step::Int` : Number of time steps. On each time step we update the forward model forecast, get model observations, and weight and resample particles.
 * `verbose::Bool` : Flag to control whether to write output
 * `output_filename::String` : Name of output file
 * `nprt::Int` : Number of particles for particle filter
 * `enable_timers::Bool` : Flag to control run time measurements
 * `particle_save_time_indices: Set of time indices to save particles at
+* `seed`: Seed to initialise state of random number generator used for filtering
 """
 Base.@kwdef struct FilterParameters{V<:Union{AbstractSet, AbstractVector}}
 
     master_rank::Int = 0
 
-    n_time_step::Int = 20
     verbose::Bool = false
 
     output_filename::String = "particle_da.h5"
@@ -28,7 +27,9 @@ Base.@kwdef struct FilterParameters{V<:Union{AbstractSet, AbstractVector}}
 
     enable_timers::Bool = false
     
-    particle_save_time_indices::V = 0:n_time_step
+    particle_save_time_indices::V = []
+    
+    seed::Union{Nothing, Int} = nothing
 
 end
 
