71fdd003a84221ad53eb51a3b3cb8f04b95d8233
Matt Graham <matthew.m.graham@gmail.com>
2023-06-08 11:28:48 +0100

    Adding job script and updating run file for Kathleen runs

---
+++ extra/weak_scaling/kathleen_weak_scaling.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+
+# Batch script to run a hybrid parallel job under SGE.
+
+# Request wallclock time (format hours:minutes:seconds).
+#$ -l h_rt=0:20:0
+
+# Request RAM per core (must be an integer)
+#$ -l mem=4G
+
+# Set the name of the job.
+#$ -N ParticleDAScaling
+
+# Select the MPI parallel environment and no. cores.
+#$ -pe mpi 160
+
+# Set the working directory to somewhere in your scratch space. 
+# This directory must exist.
+#$ -wd /home/ccaemgr/Scratch/ParticleDA.jl/extra/weak_scaling
+
+module load julia/1.9.0
+
+# Automatically set threads using ppn
+export OMP_NUM_THREADS=$(ppn)
+export JULIA_NUM_THREADS=$OMP_NUM_THREADS
+
+# Run our MPI job with the default modules. Gerun is a wrapper script for mpirun. 
+gerun julia /home/ccaemgr/Scratch/ParticleDA.jl/extra/weak_scaling/run_particleda.jl
--- extra/weak_scaling/run_particleda.jl
+++ extra/weak_scaling/run_particleda.jl
@@ -8,27 +8,33 @@ mpi_size = MPI.Comm_size(MPI.COMM_WORLD)
 
 # Save some variables for later use
 test_dir = joinpath(dirname(pathof(ParticleDA)), "..", "test")
-module_src = joinpath(test_dir, "model", "model.jl")
-input_file = joinpath(test_dir, "integration_test_1.yaml")
-truth_file = "test_observations.h5"
+llw2d_src = joinpath(test_dir, "models", "llw2d.jl")
+observation_file = "observations.h5"
+
 # Instantiate the test environment
 using Pkg
 Pkg.activate(test_dir)
 Pkg.instantiate()
 
 # Include the sample model source code and load it
-include(module_src)
-using .Model
+include(llw2d_src)
+using .LLW2d
 
 input_dict = ParticleDA.read_input_file("parametersW1.yaml")
 run_custom_params = Dict(input_dict)
 
 # Real run
 TimerOutputs.enable_debug_timings(ParticleDA)
+run_custom_params["model"]["llw2d"]["padding"] = 0
+run_custom_params["filter"]["verbose"] = true
+run_custom_params["filter"]["enable_timers"] = true
+run_custom_params["filter"]["output_filename"] = string("weak_scaling_r", mpi_size, ".h5")
+run_custom_params["filter"]["nprt"] = mpi_size * 40
 
-run_custom_params["model"]["llw2d"]["padding"]=0
-run_custom_params["filter"]["verbose"]=true
-run_custom_params["filter"]["enable_timers"]=true
-run_custom_params["filter"]["output_filename"]=string("weak_scaling_r",mpi_size,".h5")
-run_custom_params["filter"]["nprt"]=mpi_size * 64
-ParticleDA.run_particle_filter(Model.init, run_custom_params, BootstrapFilter(), truth_file)
+# Run the (optimal proposal) particle filter with simulated observations computing the
+# mean and variance of the particle ensemble. On non-Intel architectures you may need
+# to use NaiveMeanAndVarSummaryStat instead
+final_states, final_statistics = run_particle_filter(
+  LLW2d.init, run_custom_params, observation_file, OptimalFilter, ParticleDA.NaiveMeanSummaryStat
+)
+
