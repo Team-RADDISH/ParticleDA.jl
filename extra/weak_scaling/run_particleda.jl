4e76a67018ea865c699609d3e707dec387b25561
Matt Graham <matthew.m.graham@gmail.com>
2023-07-02 16:50:52 +0100

    Include # ranks in output filename and save run config

--- extra/weak_scaling/run_particleda.jl
+++ extra/weak_scaling/run_particleda.jl
@@ -2,6 +2,7 @@ using ParticleDA
 using LinearAlgebra
 using TimerOutputs
 using MPI
+using YAML
 
 # Verify BLAS implementation is OpenBLAS
 @assert occursin("openblas", string(BLAS.get_config()))
@@ -18,16 +19,29 @@ llw2d_src = joinpath(dirname(pathof(ParticleDA)), "..", "test", "models", "llw2d
 include(llw2d_src)
 using .LLW2d
 
-observation_file = "test_observations.h5"
-parameters_file = "parametersW1.yaml"
-output_file = "llw2d_filtering.h5"
+observation_file = "test_observations_$(mpi_size)_ranks.h5"
+base_parameters_file = "parametersW1.yaml"
+parameters_file = "parameters_$(mpi_size)_ranks.yaml"
+output_file = "llw2d_filtering_$(mpi_size)_ranks.h5"
 filter_type = BootstrapFilter
 summary_stat_type = NaiveMeanSummaryStat
 
 my_rank = MPI.Comm_rank(MPI.COMM_WORLD)
 
+if my_rank == 0
+    println("# MPI ranks $(mpi_size), # Julia threads = $(Threads.nthreads()), # BLAS threads = $(BLAS.get_num_threads())")
+    input_dict = YAML.load_file(base_parameters_file)
+    input_dict["filter"]["output_filename"] = output_file
+    input_dict["configuration"] = Dict(
+        "num_ranks" => mpi_size,
+        "num_threads_per_rank" => Threads.nthreads(),
+        "filter_type" => string(filter_type),
+        "summary_stat_type" => string(summary_stat_type)
+    )
+    YAML.write_file(parameters_file, input_dict)
+end
+
 if my_rank == 0 && !isfile(observation_file)
-    println("# Julia threads = $(Threads.nthreads()), # BLAS threads = $(BLAS.get_num_threads())")
     observation_sequence = simulate_observations_from_model(
       LLW2d.init, parameters_file, observation_file
     )
