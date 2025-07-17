a89684b59db1e14055fee3b0a1a33a8681e18b4f
ucabc46 <ucabc46@login22.kathleen.ucl.ac.uk>
2025-07-17 16:57:23 +0100

    add slurm script

---
+++ extra/weak_scaling/kathleen_slurm_copy_states.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#SBATCH --job-name=ParticleDAScaling
+#SBATCH --time=02:00:00
+#SBATCH --mem-per-cpu=4G
+#SBATCH --nodes=16
+#SBATCH --ntasks-per-node=1
+#SBATCH --chdir=/home/ucabc46/exp/ParticleDA.jl
+#SBATCH --output=test/slurm_log/%x-%j.out
+#SBATCH --error=test/slurm_log/%x-%j.err
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export JULIA_NUM_THREADS=$OMP_NUM_THREADS
+
+/home/ucabc46/.julia/bin/mpiexecjl -n $SLURM_NNODES\
+     julia --project=. \
+     /home/ucabc46/exp/ParticleDA.jl/test/mpi_copy_states.jl -t /home/ucabc46/exp/ParticleDA.jl/test/output/all_timers_$SLURM_NNODES.h5 -d
